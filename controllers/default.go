e873c658bded8ca51beac5a83a8f30c7ee94a6d3
包梦江 <baomengjiang@gmail.com>
2017-07-14 13:50:01 +0800

    aaa

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "try opo"
+	c.Data["Website"] = "try ppp"
 	c.Data["Email"] = "agaihghiinjjjj@gmail.com"
 	c.TplName = "index.tpl"
 }
